a635b0b95a9f1febc80867baac552820295295de
hgermishuys14 <hgermishuys14@gmail.com>
2025-06-08 15:18:00 +0000

    Initial commit

---
+++ .vscode/source/script.js
@@ -0,0 +1,13 @@
+const apiKey = 'FopdOdS3g1BZQxIzuVodKjE4imTOgUxRbSccZJuO'; 
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Process the data here
+    console.log(data);
+    // You'll access data.url for the image URL, data.title for the title, etc.
+  })
+  .catch(error => {
+    console.error('Error fetching APOD:', error);
+  });
